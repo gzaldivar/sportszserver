4d9e43fbb79c96f72ea29a1d8e139b08fcfeb0da
Gilbert Zaldivar <gilcycle@gmail.com>
2013-05-18 08:04:25 -0700

    team minor error

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,7 +2,7 @@ class TeamsController < ApplicationController
   before_filter :authenticate_user!,   only: [:new, :create, :edit, :update, :destroy, :index, :show, :getplayers, :addplayers]
   before_filter :site_owner?,           only: [:new, :create, :edit, :update, :destroy, :addplayers]
   before_filter :get_sport
-  before_filter :get_team,				only: [:edit, :update, :show, :destroy, :getplayers, :addplayers]
+  before_filter :get_team,	only: [:edit, :update, :show, :destroy, :getplayers, :addplayers]
 
 	def new
 		@team = Team.new

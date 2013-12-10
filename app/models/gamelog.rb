2bb7670680e62ae8390b584d167b5fa72e3dbf66
Gilbert Zaldivar <gilcycle@gmail.com>
2013-12-09 16:28:47 -0800

    nil stat in gamelog delete

--- app/models/gamelog.rb
+++ app/models/gamelog.rb
@@ -70,122 +70,126 @@ class Gamelog
 
         stat = FootballPassing.find(football_passing_id)
         rec = FootballReceiving.where(gameschedule_id: gameschedule_id, athlete_id: assist).first
-        if stat.yards > self.yards
-          stat.yards = stat.yards - self.yards
-        end
-        if rec.yards > self.yards
-          rec.yards = rec.yards - self.yards
-        end
-        if rec.receptions > 0
-          rec.receptions = rec.receptions - 1
-        end
-        if stat.completions > 0
-          stat.completions = stat.completions - 1
-        end
-        if stat.attempts > 0
-          stat.attempts = stat.attempts - 1
-        end
-        if self.score = "TD" 
-          if stat.td > 0
-            stat.td = stat.td - 1
+        if !stat.nil? and !rec.nil?
+          if stat.yards > self.yards
+            stat.yards = stat.yards - self.yards
           end
-          if rec.td > 0
-            rec.td = rec.td - 1
+          if rec.yards > self.yards
+            rec.yards = rec.yards - self.yards
           end
-        else
-          if stat.twopointconv > 0
-            stat.twopointconv = stat.twopointconv - 1
+          if rec.receptions > 0
+            rec.receptions = rec.receptions - 1
           end
-          if rec.twopointconv > 0
-            rec.twopointconv = rec.twopointconv - 1
+          if stat.completions > 0
+            stat.completions = stat.completions - 1
           end
+          if stat.attempts > 0
+            stat.attempts = stat.attempts - 1
+          end
+          if self.score = "TD" 
+            if stat.td > 0
+              stat.td = stat.td - 1
+            end
+            if rec.td > 0
+              rec.td = rec.td - 1
+            end
+          else
+            if stat.twopointconv > 0
+              stat.twopointconv = stat.twopointconv - 1
+            end
+            if rec.twopointconv > 0
+              rec.twopointconv = rec.twopointconv - 1
+            end
+          end
+          stat.save!
+          rec.save!
         end
-        stat.save!
-        rec.save!
-
       elsif football_rushing_id
         theplayer = Athlete.find(FootballRushing.find(football_rushing_id).athlete_id)
 
         stat = FootballRushing.find(football_rushing_id)
-        if stat.yards > self.yards
-          stat.yards -= self.yards
+        if !stat.nil?
+          if stat.yards > self.yards
+            stat.yards -= self.yards
+          end
+          if stat.attempts > 0
+            stat.attempts = stat.attempts - 1
+          end
+          if self.score = "TD" and stat.td > 0
+            stat.td = stat.td - 1
+          elsif stat.twopointconv > 0
+            stat.twopointconv = stat.twopointconv - 1
+          end
+          stat.save!
         end
-        if stat.attempts > 0
-          stat.attempts = stat.attempts - 1
-        end
-        if self.score = "TD" and stat.td > 0
-          stat.td = stat.td - 1
-        elsif stat.twopointconv > 0
-          stat.twopointconv = stat.twopointconv - 1
-        end
-        stat.save!
-
       elsif football_defense_id
         theplayer = Athlete.find(FootballDefense.find(football_defense_id).athlete_id)
         stat = FootballDefense.find(football_defense_id)
-        if stat.int_yards > self.yards
-          stat.int_yards -= self.yards
+        if !stat.nil?
+          if stat.int_yards > self.yards
+            stat.int_yards -= self.yards
+          end
+          if footballPosition == "interception" and stat.interception > 0
+            stat.interceptions -= 1
+          elsif stat.fumbles_recovered > 0
+            stat.fumbles_recovered -=1
+          end
+          if self.score = "TD" and stat.int_td > 0
+            stat.int_td -= 1
+          elsif stat.safety > 0
+            stat.safety -= 1
+          end
+          stat.save!#        elsif isReturner?
         end
-        if footballPosition == "interception" and stat.interception > 0
-          stat.interceptions -= 1
-        elsif stat.fumbles_recovered > 0
-          stat.fumbles_recovered -=1
-        end
-        if self.score = "TD" and stat.int_td > 0
-          stat.int_td -= 1
-        elsif stat.safety > 0
-          stat.safety -= 1
-        end
-        stat.save!#        elsif isReturner?
-
       elsif football_returner_id
         theplayer = Athlete.find(FootballReturner.find(football_returner_id).athlete_id)
 
         rettype = logentry.split(" ")
         stat = FootballReturner.find(football_returner_id)
-
-        if rettype[0] == "punt"
-          if stat.punt_returnlong == stat.punt_returnyards
-            stat.punt_returnlong =0
+        if !stat.nil?
+          if rettype[0] == "punt"
+            if stat.punt_returnlong == stat.punt_returnyards
+              stat.punt_returnlong =0
+            end
+            if stat.punt_returnyards > self.yards
+              stat.punt_returnyards -= self.yards
+            end
+            if stat.punt_return > 0
+              stat.punt_return -= 1
+            end
+            if self.score = "TD" and stat.punt_returntd > 0
+              stat.punt_returntd -= 1
+            end
+          else
+            if stat.kolong == stat.koyards
+              stat.kolong =0
+            end
+            if stat.koyards > self.yards
+              stat.koyards -= self.yards
+            end
+            if stat.koreturns > 0
+              stat.koreturns -= 1
+            end
+            if self.score = "TD" and stat.kotd > 0
+              stat.kotd -= 1
+            end
           end
-          if stat.punt_returnyards > self.yards
-            stat.punt_returnyards -= self.yards
-          end
-          if stat.punt_return > 0
-            stat.punt_return -= 1
-          end
-          if self.score = "TD" and stat.punt_returntd > 0
-            stat.punt_returntd -= 1
-          end
-        else
-          if stat.kolong == stat.koyards
-            stat.kolong =0
-          end
-          if stat.koyards > self.yards
-            stat.koyards -= self.yards
-          end
-          if stat.koreturns > 0
-            stat.koreturns -= 1
-          end
-          if self.score = "TD" and stat.kotd > 0
-            stat.kotd -= 1
-          end
+          stat.save!
         end
-        stat.save!
-
       elsif football_place_kicker_id
         theplayer = Athlete.find(FootballPlaceKicker.find(football_place_kicker_id).athlete_id)
         stat = FootballPlaceKicker.find(football_place_kicker_id)
+        if !stat.nil?
+          if self.score == "FG"
+            stat.fgmade -= 1
+            stat.fgattempts -=1
+          elsif self.score == "XP"
+            stat.xpmade -= 1
+            stat.xpattempts -= 1
+          end
 
-        if self.score == "FG"
-          stat.fgmade -= 1
-          stat.fgattempts -=1
-        elsif self.score == "XP"
-          stat.xpmade -= 1
-          stat.xpattempts -= 1
+          stat.save!
         end
-
-        stat.save!
       end
 
       adjustgamescore

8feee4df457057b7e92314c988bde53a81154eb7
Gilbert Zaldivar <gilcycle@gmail.com>
2014-06-11 07:56:40 -0700

    set up mailer for gametrackerpro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,9 +57,9 @@ Sportzserver::Application.configure do
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => 'smtpout.secureserver.net',
-    :domain  => 'www.gametrackerpro.com',
+    :domain  => 'www.eazesportz.com',
     :port      => 80,
-    :user_name => 'info@gametrackerpro.com',
+    :user_name => 'info@eazesportz.com',
     :password => 'Cjz121699',
     :authentication => :plain
   }

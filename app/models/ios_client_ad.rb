4fcfc44b398695a431df8761a5f1c4c135e77caa
Gilbert Zaldivar <gilcycle@gmail.com>
2014-06-13 19:03:59 -0700

    add image to ios ads

--- app/models/ios_client_ad.rb
+++ app/models/ios_client_ad.rb
@@ -1,5 +1,6 @@
 class IosClientAd
 	include Mongoid::Document
+	include Mongoid::Paperclip
 
 	field :referencename, type: String
 	field :productid, type: String
@@ -8,4 +9,16 @@ class IosClientAd
 	field :price, type: Float, default: 0.0
 
 	belongs_to :admin
+
+	has_mongoid_attached_file :iosadimage,
+	    :storage        => :s3,
+	    :s3_credentials => { bucket: S3DirectUpload.config.bucket,
+	                         access_key_id: S3DirectUpload.config.access_key_id,
+	    					           secret_access_key: S3DirectUpload.config.secret_access_key },
+	    :styles => {
+	      	:tiny     => ['50x50', :jpg],
+	      	:thumb    => ['125x125',   :jpg],
+	      	:medium   => ['300x250',    :jpg],
+	    }
+
 end
--- app/views/ios_client_ads/show.json.rabl
+++ app/views/ios_client_ads/show.json.rabl
@@ -1,3 +1,6 @@
 object @product
 node(:id) { |o| o.id.to_s }
 attributes :referencename, :productid, :adtype, :appleid, :price
+node(:thumb) { |t| t.iosadimage(:thumb) }
+node(:tiny) { |t| t.iosadimage(:tiny) }
+node(:medium) { |t| t.iosadimage(:medium) }

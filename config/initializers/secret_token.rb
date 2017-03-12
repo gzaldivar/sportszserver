4f45c207fb5cd236fa6ff18f8cbb2293f69c49fc
Gilbert Zaldivar <gzaldivar@icloud.com>
2017-03-12 14:56:51 -0700

    Delete secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Sportzserver::Application.config.secret_token = '7537e68983784ce48084651e5385273a18f54f6071d4e3cd4a413c314090d5a77287f38dc8517c6ce8f4cb92e51fd4593037b4d3cb4dfe422570d38e5cb4a02b'
